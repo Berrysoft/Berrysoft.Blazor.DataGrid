c7672dd2559ac085c80c333c8550c5b2a93bc569
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-02 23:22:11 +0800

    Add code.

---
+++ Berrysoft.Blazor.DataGrid/DataGridBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace Berrysoft.Blazor.DataGrid
+{
+    public class DataGridBase : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable? Items { get; set; }
+
+        protected IEnumerable? DisplayItems { get; set; }
+
+        [Parameter]
+        public RenderFragment? Headers { get; set; }
+
+        public event EventHandler<string>? ColumnSorted;
+
+        protected override void OnParametersSet()
+        {
+            DisplayItems = Items;
+        }
+
+        public void SetColumnSortDirection(string column, DataGridSortDirection direction, IComparer? comparer)
+        {
+            if (direction != DataGridSortDirection.None)
+            {
+                DisplayItems = Items?.OfType<object>()?.OrderBy(item => item.GetType().GetProperty(column).GetValue(item), direction, comparer);
+            }
+            else
+            {
+                DisplayItems = Items;
+            }
+            ColumnSorted?.Invoke(this, column);
+            StateHasChanged();
+        }
+    }
+}
---
+++ Berrysoft.Blazor.DataGrid/DataGridSortDirection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Berrysoft.Blazor.DataGrid
+{
+    public enum DataGridSortDirection
+    {
+        None,
+        Ascending,
+        Descending
+    }
+
+    static class DataGridSortHelper
+    {
+        public static IEnumerable<T>? OrderBy<T, TKey>(this IEnumerable<T>? source, Func<T, TKey> keySelector, DataGridSortDirection direction, IComparer? comparer)
+        {
+            return direction switch
+            {
+                DataGridSortDirection.Ascending => source.OrderBy(keySelector, new ComparerWrapper<TKey>(comparer)),
+                DataGridSortDirection.Descending => source.OrderByDescending(keySelector, new ComparerWrapper<TKey>(comparer)),
+                _ => source
+            };
+        }
+    }
+
+    class ComparerWrapper<T> : IComparer<T>
+    {
+        private readonly IComparer comparer;
+
+        public ComparerWrapper(IComparer? comparer) => this.comparer = comparer ?? Comparer<T>.Default;
+
+        public int Compare(T x, T y) => comparer.Compare(x, y);
+    }
+}
